f199de6c1f840a775a3f8bc6267fa2a49cef392b
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-09-16 15:42:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -401,8 +401,8 @@ def rex():
         prompt_count = user_prompt_count.get(session_id, 0)
 
         # Check if the user has exceeded the daily limit
-        if prompt_count >= 5:
-            return jsonify({'answer': "NOTIFICATION!!!: Sorry, you've completed your 10 prompts for today."}), 200
+        if prompt_count >= 1:
+            return jsonify({'answer': "NOTIFICATION!!!: Sorry, it looks like you've hit your message limit. The free trial allows for only 7 messages. <a href='https://www.google.com/'>Click here to continue with a weekly or monthly plan</a"}), 200
 
         # Generate the chat response
         res = {}
