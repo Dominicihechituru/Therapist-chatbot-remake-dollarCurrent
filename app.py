b11fe40dd972ac97efdef586c885fdb442060186
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-09-24 14:49:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -500,7 +500,7 @@ def rex():
         prompt_count = user_prompt_count.get(session_id, 0)
         res = {}
         # Check if the user has exceeded the daily limit
-        if prompt_count >= 2 and (check_subscription_status(subscription_code) != "active" and check_subscription_status(subscription_code) != "completed" and check_subscription_status(subscription_code) != "expired", check_subscription_status(subscription_code) != "contactsupport):
+        if prompt_count >= 2 and (check_subscription_status(subscription_code) != "active" and check_subscription_status(subscription_code) != "completed" and check_subscription_status(subscription_code) != "expired" and check_subscription_status(subscription_code) != "contactsupport):
             return jsonify({'answer': "NOTIFICATION!!!: Sorry, it looks like you've hit your message limit. The free trial allows for only 7 messages. <a href='https://www.google.com/'>Click here to continue with a weekly or monthly plan</a"}), 200
         if prompt_count >= 2 and check_subscription_status(subscription_code) == "active":
             # Generate the chat response
