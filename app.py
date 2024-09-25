279ab77d7824d91ca08fa6c3c3ae9bf08cbe3efb
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-09-25 22:42:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ def payment():
     return render_template('payment.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
