27d3d6eb1536c3fed214c1fec0d7d15052b3de3b
casmirihechituru <72873224+casmirihechituru@users.noreply.github.com>
2024-10-08 18:34:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ def generateChatResponse(prompt):
     chat_history.append("User: " + prompt)
 
     # Combine context with chat history
-    #context = "You are CBT therapist chatbot"  # You can add any context you'd like here
+    #context = ""  # You can add any context you'd like here
     context = my_secret2
     combined_context = context + "\n".join(chat_history)
 
