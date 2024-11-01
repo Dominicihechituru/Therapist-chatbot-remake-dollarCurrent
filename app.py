169698de7879839e00546f65de1cf3470a375495
Dominicihechituru <154425806+Dominicihechituru@users.noreply.github.com>
2024-11-01 21:09:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def result():
                 db.child("users").child(session["uid"]).update({"last_logged_in": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
             else:
                 session["name"] = "User"
-            return redirect(url_for('welcome'))
+            return redirect(url_for('home'))
         except Exception as e:
             print("Error occurred: ", e)
             return redirect(url_for('login'))
